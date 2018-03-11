fdaeb4c44e98569d2a50ab3b7e1cad0c695ed26d
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2018-03-11 12:08:06 +0530

    updating

---
+++ mission/src/main/java/com/microservices/mission/model/Mission.java
@@ -0,0 +1,46 @@
+package com.microservices.mission.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="missions")
+public class Mission {
+	
+	@Id
+	private String ID;
+	
+	private String name;
+	private String description;
+	private int pointValue;
+	
+	
+	public String getID() {
+		return ID;
+	}
+	
+	public void setID(String ID) {
+		this.ID=ID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPointValue() {
+		return pointValue;
+	}
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
+	
+	
+
+}
